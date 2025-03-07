8ee2bc6cc13873f598f6ae2876985e09e749796a
Alex Muir <wamuir1212@protonmail.com>
2025-03-06 20:12:09 -0800

    Completed the print_weather function in helpers.py. The data retrieved using the API was broken down into easy to understand variables with conversions being made as necessary, then printed.

--- helpers.py
+++ helpers.py
@@ -1,5 +1,7 @@
 import json
 import requests
+from datetime import datetime
+import time
 
 FILENAME = 'settings.json'
 
@@ -47,8 +49,8 @@ def set_settings(settings):
             save_settings(settings)                                             
             break                                                               
         else:                                                                   
-            print("Please enter a valid selection")                             
-                                                                                
+            print("Please enter a valid selection")  
+                                                                                  
     return settings
 
 
@@ -80,5 +82,65 @@ def fetch_weather(settings):
     """Fetch current weather from OpenWeatherMaps based on zip code"""                                                                     
     weather_address = f"https://api.openweathermap.org/data/2.5/weather?zip={settings["zip_code"]},us&appid={settings["API_KEY"]}&units=imperial"
     r = requests.get(weather_address)                                           
-    weather_dict = r.json()                                                     
+    weather_dict = r.json()
+    print_weather(weather_dict)
 
+def print_weather(weather_dict):
+    """Take relevant information from data returned by the API and print it"""
+    
+    # Get weather description
+    weather_list = weather_dict['weather']
+    weather_desc = weather_list[0]['description']
+
+    # Get cloud coverage
+    cloud_dict = weather_dict['clouds']
+    cloud_coverage = cloud_dict['all']
+
+    # Get temp related data
+    temp_dict = weather_dict['main']
+    current_temperature = temp_dict['temp']
+    feels_like = temp_dict['feels_like']
+    min_temp = temp_dict['temp_min']
+    max_temp = temp_dict['temp_max']
+
+    # Get sunrise and sunset times  
+    sys_dict = weather_dict['sys']
+    sunrise_unix_UTC = sys_dict['sunrise']
+    sunset_unix_UTC = sys_dict['sunset']
+    readable_sunrise = datetime.fromtimestamp(sunrise_unix_UTC).time()
+    readable_sunset = datetime.fromtimestamp(sunset_unix_UTC).time()
+
+    # Get humidity and visability
+    humidity = temp_dict['humidity']
+    visibility = weather_dict['visibility']
+
+    # Get wind speed and direction
+    wind_dict = weather_dict['wind']
+    wind_speed = wind_dict['speed']
+    wind_direction = wind_dict['deg']
+    
+    # Get time of data calculation
+    time_of_calc_unix_UTC = weather_dict['dt']
+    readable_time_of_calc = datetime.fromtimestamp(time_of_calc_unix_UTC)
+
+    # Print everything
+    print(f"\n{'CURRENT WEATHER':>22}")
+    print("-" * 29)
+   
+    print(f"Date: {datetime.today().date()}") 
+    print(f"Description: {weather_desc}")
+    print(f"Current Temperature: {current_temperature}")
+    print(f"Feels Like: {feels_like}")
+    print(f"Min Temperature: {min_temp}")
+    print(f"Max Temperature: {max_temp}")
+    print(f"Humidity: {humidity}%")
+    print(f"Wind Speed: {wind_speed}mph")
+    print(f"Wind Direction: {wind_direction}")
+    print(f"Cloud Coverage: {cloud_coverage}%")
+    print(f"Visibility: {visibility/1000.0}km")
+    print(f"Sunrise: {readable_sunrise}")
+    print(f"Sunset: {readable_sunset}")
+    print(f"\nTime of Data Calculation: {readable_time_of_calc}")
+
+    print("-" * 29)
+    
